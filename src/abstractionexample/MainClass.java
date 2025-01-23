0db59aa2fef5ad304a23b62488b70896819e24b7
MohammedAbumasood <abumasood657@gmail.com>
2025-01-23 04:35:53 -0800

    two line were added

--- src/abstractionexample/MainClass.java
+++ src/abstractionexample/MainClass.java
@@ -3,7 +3,8 @@ package abstractionexample;
 public class MainClass {
     public static void main(String[] args) {
         AbstractChildClass abstractChildClass = new AbstractChildClass();
-
+        // extra line added
+        // for git hub to know
         abstractChildClass.demo();
         abstractChildClass.demo2();
     }
